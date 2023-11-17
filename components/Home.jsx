5240388e483e24471149dba577a2e1719d26b4e8
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2023-11-17 14:54:31 -0600

    added my name

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(props){
         <div className="home-container">
             <img src={yellowIcon} className="yellowIcon" />
             <div className="home-content">
-                <h1 className="home-content-heading">Quizzical</h1>
+                <h1 className="home-content-heading">Quizzical - by Prajwal</h1>
                 <h5 className="home-content-description">Welcome to quizzical. Lets test your general knowledge!</h5>
                 <button className="startButton" onClick={props.buttonClick}>Start quiz</button>
             </div>
